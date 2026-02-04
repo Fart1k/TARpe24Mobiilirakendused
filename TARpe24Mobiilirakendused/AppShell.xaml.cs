68f1aaa459bea2907815ac57d90f5f9cb66e0c9c
opilane <opilane@HP-CZC2349HTN>
2026-02-04 09:19:45 +0200

    Add project files.

---
+++ TARpe24Mobiilirakendused/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TARpe24Mobiilirakendused
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TARpe24Mobiilirakendused/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace TARpe24Mobiilirakendused
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void ResetClicks(object? sender, EventArgs e)
+        {
+            count = 0;
+
+            CounterBtn.Text = $"Clicked 0 time";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
